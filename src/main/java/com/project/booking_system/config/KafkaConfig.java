c9b92017e971e840e1e9d3f3926ebf35f7fa3baf
haryo <emadekuncoro@gmail.com>
2025-10-23 11:19:38 +0700

    fix(kafka): fix kafka serialization remove security lib for testing

--- src/main/java/com/project/booking_system/config/KafkaConfig.java
+++ src/main/java/com/project/booking_system/config/KafkaConfig.java
@@ -1,8 +1,8 @@
 package com.project.booking_system.config;
 
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -25,7 +25,7 @@ public class KafkaConfig {
     public ProducerFactory<String, String> producerFactory() {
         Map<String,Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(props);
     }
