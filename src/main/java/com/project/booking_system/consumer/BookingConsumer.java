57329500b4a997d9a5472bc784a30b8c61ba3d9c
haryo <emadekuncoro@gmail.com>
2025-10-23 11:33:47 +0700

    fix(booking): fix booking flow to handle json message

--- src/main/java/com/project/booking_system/consumer/BookingConsumer.java
+++ src/main/java/com/project/booking_system/consumer/BookingConsumer.java
@@ -1,29 +1,41 @@
 package com.project.booking_system.consumer;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.booking_system.config.KafkaConfig;
+import com.project.booking_system.entity.Booking;
 import com.project.booking_system.repository.BookingRepository;
 import com.project.booking_system.service.BookingService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service @Log4j2
 public class BookingConsumer {
     private final BookingRepository bookingRepo;
     private final BookingService bookingService;
+    private final ObjectMapper objectMapper;
 
     public BookingConsumer(BookingRepository bookingRepo,
-                           BookingService bookingService){
+                           BookingService bookingService, ObjectMapper objectMapper){
         this.bookingRepo=bookingRepo;
         this.bookingService=bookingService;
+        this.objectMapper = objectMapper;
     }
 
     @KafkaListener(topics= KafkaConfig.BOOKING_TOPIC,groupId="booking-group")
-    public void consume(String message){
-        log.info("receive kafka ={} ", message);
-        Long bookingId = Long.parseLong(message);
-        bookingRepo.findById(bookingId).ifPresent(b->{
-            bookingService.processBooking(b);
-        });
+    public void consume(String jsonMessage) {
+        log.info("Receive JSON Kafka: {}", jsonMessage);
+        try {
+            Map<String, Object> data = objectMapper.readValue(jsonMessage, new TypeReference<Map<String, Object>>() {});
+            Long bookingId = ((Number) data.get("bookingId")).longValue();
+            Booking booking = bookingRepo.findById(bookingId).orElse(null);
+            bookingService.processBooking(booking);
+
+        } catch (Exception e) {
+            log.error("fail to consume kafka and process booking ={}", jsonMessage, e);
+        }
     }
 }
--- src/main/java/com/project/booking_system/service/BookingPublisher.java
+++ src/main/java/com/project/booking_system/service/BookingPublisher.java
@@ -1,21 +1,35 @@
 package com.project.booking_system.service;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.booking_system.config.KafkaConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.util.HashMap;
+import java.util.Map;
+
+@Service @Slf4j
 public class BookingPublisher {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
-    public BookingPublisher(KafkaTemplate<String, String> kafkaTemplate) {
+    public BookingPublisher(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
-    public void publishBooking(Long productId, int quantity) {
-        String message = productId + ":" + quantity;
-        kafkaTemplate.send(KafkaConfig.BOOKING_TOPIC, message);
+    public void publishBooking(Long bookingId, int quantity) {
+        Map<String, Object> bookingData = new HashMap<>();
+        bookingData.put("bookingId", bookingId);
+        bookingData.put("quantity", quantity);
+        try {
+            String jsonMessage = objectMapper.writeValueAsString(bookingData);
+            kafkaTemplate.send(KafkaConfig.BOOKING_TOPIC, jsonMessage);
+        } catch (Exception e) {
+            log.error("fail to publish booking {} ", bookingData, e);
+        }
     }
 }
